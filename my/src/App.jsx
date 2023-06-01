9ee2104d48488ed5f253783ac554c6671982b907
amanjn1122 <amanjn1122@gmail.com>
2023-06-01 14:29:24 +0530

    added service component

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import Services from "./components/Services";
 
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
+// import "./styles/services.scss";
 
 function App() {
   return(
@@ -19,6 +21,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/contact" element={<Contact/>}/>
+        <Route exact path="/services" element={<Services/>}></Route>
         <Route exact path="*" element={<div>not found</div>}/>
       </Routes>
       <Footer/>
--- my/src/components/Contact.jsx
+++ my/src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
             <form>
                 <div>
                     <label >Name</label>
-                    <input type="text" required placeholder='abc'/>
+                    <input type="text" required placeholder='abc' autoFocus/>
                 </div>
 
                 <div>
